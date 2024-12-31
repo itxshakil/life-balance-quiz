2ba8e220476830d80c9fef2124925e9c29c1f8cd
itxnargis <itxnargiskhatun@gmail.com>
2024-12-31 17:41:47 +0530

    Add build

---
+++ public/build/assets/DeleteUserForm-_jpaQelU.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DeleteUserForm.vue_vue_type_script_setup_true_lang-Cxd9uwTJ.js";import"./app-B55S8HqD.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./TextInput.vue_vue_type_script_setup_true_lang-Brv5-JKK.js";export{o as default};
---
+++ public/build/assets/UpdateProfileInformationForm-BE2pL_n7.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./UpdateProfileInformationForm.vue_vue_type_script_setup_true_lang-CXw7B7ue.js";import"./app-B55S8HqD.js";import"./TextInput.vue_vue_type_script_setup_true_lang-Brv5-JKK.js";import"./PrimaryButton-FUoB4Uqt.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
