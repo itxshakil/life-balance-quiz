068b071ccece9729bdbe7c65d4ed73f78dc1b174
Shakil Alam <itxshakil@gmail.com>
2024-12-31 23:56:47 +0530

    Remove Ad

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -65,18 +65,6 @@
             })(window, document, "clarity", "script", "pmlfn3bdho");
         </script>
 
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4132498105758259"
-     crossorigin="anonymous"></script>
-        <!-- Life Balance Quiz -->
-        <ins class="adsbygoogle"
-             style="display:block"
-             data-ad-client="ca-pub-4132498105758259"
-             data-ad-slot="4471724949"
-             data-ad-format="auto"
-             data-full-width-responsive="true"></ins>
-        <script>
-             (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
     </head>
     <body class="font-sans antialiased">
         @inertia
