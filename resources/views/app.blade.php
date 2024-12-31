c190cc0e8ed70807f50a971da420533d6e2e6e16
Shakil Alam <itxshakil@gmail.com>
2024-12-31 18:40:42 +0530

    Add GA

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -46,6 +46,16 @@
         @routes
         @vite(['resources/js/app.ts', "resources/js/Pages/{$page['component']}.vue"])
         @inertiaHead
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-44QE5R8BSJ"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-44QE5R8BSJ');
+        </script>
     </head>
     <body class="font-sans antialiased">
         @inertia
