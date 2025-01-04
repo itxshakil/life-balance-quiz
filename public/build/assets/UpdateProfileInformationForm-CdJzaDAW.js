2ffc74c3fd20d8f67826c1ee3e9053175ea8fff4
Shakil Alam <itxshakil@gmail.com>
2025-01-05 00:11:32 +0530

    feat: Build assets

---
+++ public/build/assets/UpdateProfileInformationForm-CdJzaDAW.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./UpdateProfileInformationForm.vue_vue_type_script_setup_true_lang-AJzBvKC_.js";import"./app-CAX61PJj.js";import"./TextInput.vue_vue_type_script_setup_true_lang-CrPgN5np.js";import"./PrimaryButton-bYw1_ahk.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
