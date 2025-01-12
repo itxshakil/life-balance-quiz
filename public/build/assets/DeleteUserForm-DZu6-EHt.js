e7baebfa2a93d691d8c20b0455144fc921b86d03
Shakil Alam <itxshakil@gmail.com>
2025-01-12 15:54:41 +0530

    feat: Add new build and new service worker

---
+++ public/build/assets/DeleteUserForm-DZu6-EHt.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./DeleteUserForm.vue_vue_type_script_setup_true_lang-ChHKRdKh.js";import"./app-wpJW6bKR.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";import"./TextInput.vue_vue_type_script_setup_true_lang-5bqlos1o.js";export{o as default};
---
+++ public/build/assets/UpdateProfileInformationForm-D3PNInuc.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./UpdateProfileInformationForm.vue_vue_type_script_setup_true_lang-BhNIqSlT.js";import"./app-wpJW6bKR.js";import"./TextInput.vue_vue_type_script_setup_true_lang-5bqlos1o.js";import"./PrimaryButton-JqnZB9-O.js";import"./_plugin-vue_export-helper-DlAUqK2U.js";export{o as default};
